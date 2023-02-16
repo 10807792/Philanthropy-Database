297436f1a02b9c44e558104a3837b5d62fc86697
Innobys <bigredquinlan@gmail.com>
2023-02-15 22:38:15 -0700

    Added new cover photo and changed code for it.

--- app.js
+++ app.js
@@ -73,7 +73,7 @@ function show_home(){
     tag("canvas").innerHTML=` 
     <div class="center-screen">
     
-    <p><img height="${window.innerHeight * .6}" src="images/brookers-logo.png"></p>
+    <p><img height="${window.innerHeight * .6}" src="images/PhilanthropyOpeningPhoto.png"></p>
     <div style="text-align:center"></div>
     
     
