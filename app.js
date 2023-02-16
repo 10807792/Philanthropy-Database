32f093c7f301982235ff120afcc1537c2cefa77b
Innobys <bigredquinlan@gmail.com>
2023-02-15 23:02:44 -0700

    Deleted old logo & added new one w/ no background

--- app.js
+++ app.js
@@ -73,7 +73,7 @@ function show_home(){
     tag("canvas").innerHTML=` 
     <div class="center-screen">
     
-    <p><img height="${window.innerHeight * .6}" src="images/PhilanthropyOpeningPhoto.png"></p>
+    <p><img height="${window.innerHeight * .6}" src="images/pngegg.png"></p>
     <div style="text-align:center"></div>
     
     
